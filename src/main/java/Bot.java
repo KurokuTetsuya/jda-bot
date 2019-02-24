98fb1377dea8ce2d61930161b6d863793564e943
KurokuTetsuya <45705890+KurokuTetsuya@users.noreply.github.com>
2019-02-24 20:47:48 +0700

    Update Bot.java

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -8,7 +8,7 @@ public class Bot {
     public static String prefix = "g!";
         public static void main (String[] args) throws Exception {
      JDA jda;
-      jda = new JDABuilder("NTQ4MzU3MDI0MTE1NTg5MTIw.D1ES3Q.i3t7WYrpLR56bFWPCmBBoLwVClw").build();
+      jda = new JDABuilder("token").build();
       jda.addEventListener(new HelloEvent());
       jda.addEventListener(new Fun());
     }
